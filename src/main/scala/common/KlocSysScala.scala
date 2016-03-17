4bb524f223c5fdd817a7a04a78962bfb92959e8d
Shilpika S <sshilpika5@gmail.com>
2016-03-16 20:53:19 -0500

    commits date change in log

--- src/main/scala/common/KlocSysScala.scala
+++ src/main/scala/common/KlocSysScala.scala
@@ -26,7 +26,7 @@ object KlocSysScala extends IO with GitBashExec{
     //println(deleteCode+" DCODE")
 
 
-    val exitCode = Seq("/bin/sh", "-c", "cd literalinclude-scala && git log --shortstat --pretty=format:\"metrics@cs.luc.edu %H - %an - %ad - %cn - %cd - %s\" | grep \"metrics@cs.luc.edu \" | " +
+    val exitCode = Seq("/bin/sh", "-c", "cd literalinclude-scala && git log --shortstat --pretty=format:\"metrics@cs.luc.edu %H %cI metrics-dash %an - %ad - %cn - %cd - %s\" | grep \"metrics@cs.luc.edu \" | " +
       "awk '{gsub(\"metrics@cs.luc.edu\",\"\");print}' > log1.txt").!!
 
     val conf = new SparkConf().setMaster("local").setAppName("Spark Git Checkout")
@@ -36,11 +36,15 @@ object KlocSysScala extends IO with GitBashExec{
 
     val result = gitLogSpark.foreach(x =>{
       println("This is X!!!!!!"+x)
-      val y = x.split(" ")(1)
-      println("This is the SHA code:::"+y)
+      val y = x.split(" ")
+      val sha = y(1)
+      val date = y(2)
+      println("This is the SHA code:::"+sha)
+      println("This is the the commit date:::"+date)
       //val checkout = Seq("/bin/sh", "-c", "cd literalinclude-scala && git checkout "+y).!
-      Seq("/bin/sh", "-c", "cd literalinclude-scala && git checkout "+y+ " && cloc --report_file=res2.txt ../literalinclude-scala/ && grep \"SUM\" res2.txt >> log100.txt").! //&& " +
+      Seq("/bin/sh", "-c", "cd literalinclude-scala && git checkout "+sha+ " && cloc --by-file --report_file=res2.txt ../literalinclude-scala/ && grep \"../\" res2.txt >> ~/log100.txt").! //&& " +
         //"awk '{gsub(\"SUM \",\"\");print}' >> log100.txt").!!
+      //val gitLogStore = sc.textFile("~/log100.txt")
       //println("THE ZZZZZ "+z)
     })
    // result
