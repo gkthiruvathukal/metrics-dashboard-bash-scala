fe0df090f53c7090a8ef91e486f639d3eb19a69d
Shilpika S <sshilpika5@gmail.com>
2016-03-16 23:52:52 -0500

    removing unwanted files again

--- src/main/scala/common/KlocSysScala.scala
+++ src/main/scala/common/KlocSysScala.scala
@@ -19,19 +19,19 @@ object KlocSysScala extends IO with GitBashExec{
     val gitCloneExitCode = gitCloneExec(cloneCommand)
     //println(gitCloneExitCode+"gitCloneExitCode")
 
-    val gitLogCode = gitCommitsListExec(gitCommitsList("linux"))
+    val gitLogCode = gitCommitsListExec(gitCommitsList("literalinclude-scala"))
     println("GIT LOG "+gitLogCode)
     println("DELETINF")
-    //val deleteCode = Seq("/bin/sh","-c","rm -rf linux").!!
+    //val deleteCode = Seq("/bin/sh","-c","rm -rf literalinclude-scala").!!
     //println(deleteCode+" DCODE")
 
 
-    val exitCode = Seq("/bin/sh", "-c", "cd linux && git log --shortstat --pretty=format:\"metrics@cs.luc.edu %H %cI metrics-dash %an - %ad - %cn - %cd - %s\" | grep \"metrics@cs.luc.edu \" | " +
+    val exitCode = Seq("/bin/sh", "-c", "cd literalinclude-scala && git log --shortstat --pretty=format:\"metrics@cs.luc.edu %H %cI metrics-dash %an - %ad - %cn - %cd - %s\" | grep \"metrics@cs.luc.edu \" | " +
       "awk '{gsub(\"metrics@cs.luc.edu\",\"\");print}' > log1.txt").!!
 
     val conf = new SparkConf().setMaster("local").setAppName("Spark Git Checkout")
     val sc = new SparkContext(conf)
-    val gitLogSpark = sc.textFile("linux/log1.txt")
+    val gitLogSpark = sc.textFile("literalinclude-scala/log1.txt")
     println("XXXXX")
 
     val result = gitLogSpark.foreach(x =>{
@@ -42,14 +42,14 @@ object KlocSysScala extends IO with GitBashExec{
       println("This is the SHA code:::"+sha)
       println("This is the the commit date:::"+date)
 
-      //val checkout = Seq("/bin/sh", "-c", "cd linux && git checkout "+y).!
-      Seq("/bin/sh", "-c", "cd linux && git checkout "+sha+ " && cloc --by-file --report_file=res2.txt ../linux/ && grep \"../\" res2.txt >> ~/log100.txt && echo "+date+" >> ~/log100.txt" ).! //&& " +
+      //val checkout = Seq("/bin/sh", "-c", "cd literalinclude-scala && git checkout "+y).!
+      Seq("/bin/sh", "-c", "cd literalinclude-scala && git checkout "+sha+ " && cloc --by-file --report_file=res2.txt ../literalinclude-scala/ && grep \"../\" res2.txt >> ~/log100.txt && echo "+date+" >> ~/log100.txt" ).! //&& " +
         //"awk '{gsub(\"SUM \",\"\");print}' >> log100.txt").!!
-      //Seq("/bin/sh", "-c", "cd linux && echo "+date+" >>~/log100.txt").!
+      //Seq("/bin/sh", "-c", "cd literalinclude-scala && echo "+date+" >>~/log100.txt").!
       //val gitLogStore = sc.textFile("~/log100.txt")
       //println("THE ZZZZZ "+z)
     })
-    val deleteCode = Seq("/bin/sh","-c","rm -rf linux").!!
+    val deleteCode = Seq("/bin/sh","-c","rm -rf literalinclude-scala").!!
    // result
     println("THE TOTAL COMPUTATION TIME IS :"+(System.currentTimeMillis()-t1))
 
