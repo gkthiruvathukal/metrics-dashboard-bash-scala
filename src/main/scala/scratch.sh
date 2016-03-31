32191d750dfe450a75f1e1353419c6ed145027d7
Shilpika S <sshilpika5@gmail.com>
2016-03-31 18:51:06 -0500

    removing scratch from gitignore

---
+++ src/main/scala/scratch.sh
@@ -0,0 +1,27 @@
+#! /bin/bash/
+
+echo "Ok, let me start..."
+
+args=("$@")
+
+echo ${args[0]} 'is the sha for the cloned repo'
+sha=${args[0]}
+reponame=${args[1]}
+branchname=${args[2]}
+
+if [ ! -d commitsMetrics ]; then
+    mkdir commitsMetrics
+fi
+
+cd commitsMetrics
+mkdir ${sha}
+cd ${sha}
+
+git init
+git remote add parentNode /home/shilpika/scratch/metrics-dashboard-bash-scala/${reponame}
+git pull parentNode ${branchname}
+git reset --hard ${sha}
+
+cloc --by-file --report-file=./clocByFile.txt .
+
+
