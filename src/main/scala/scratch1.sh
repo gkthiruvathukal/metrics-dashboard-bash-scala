1bf29c1a78ced7823e0c0373d84dfb59fc0ad665
Shilpika S <sshilpika5@gmail.com>
2016-06-10 17:18:54 -0500

    changing main

--- src/main/scala/scratch1.sh
+++ src/main/scala/scratch1.sh
@@ -1,3 +1,28 @@
 #! /bin/bash
 
-echo "OK.. I'm in scratch!!!"
+echo "Ok, let me start..."
+
+args=("$@")
+
+echo ${args[0]} 'is the sha for the cloned repo'
+sha=${args[0]}
+reponame=${args[1]}
+branchname=${args[2]}
+username=${args[3]}
+
+cd /scratch/
+
+# base folder
+if [ ! -d sshilpika ]; then
+    mkdir sshilpika
+fi
+
+cd sshilpika
+
+# cloc
+if [ ! -d cloc ]; then
+     git clone https://github.com/AlDanial/cloc.git
+fi
+
+# repo
+echo https://github.com/${username}/${reponame}.git
